955c2722f4a2f93234c9b917ba168f84c736eeee
joelsnz <joel04slm@gmail.com>
2025-03-03 11:41:03 +0100

    Add ;

--- include/registers.h
+++ include/registers.h
@@ -6,28 +6,28 @@ struct registers {
     struct {
       uint8_t a;
       uint8_t f;
-    }
+    };
     uint16_t af;  
   };
   union {
     struct {
       uint8_t b;
       uint8_t c;
-    }
+    };
     uint16_t bc;  
   };
   union {
     struct {
       uint8_t d;
       uint8_t e;
-    }
+    };
     uint16_t de;  
   };
   union {
     struct {
       uint8_t h;
       uint8_t l;
-    }
+    };
     uint16_t hl;  
   };
   uint8_t sp;
