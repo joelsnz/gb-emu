5ec42326e6d4ccc8b83c639885c0f73d69310649
joelsnz <joel04slm@gmail.com>
2025-03-03 11:37:18 +0100

    Registers struct

---
+++ include/registers.h
@@ -0,0 +1,37 @@
+#ifndef REGISTERS
+#define REGISTERS
+
+struct registers {
+  union {
+    struct {
+      uint8_t a;
+      uint8_t f;
+    }
+    uint16_t af;  
+  };
+  union {
+    struct {
+      uint8_t b;
+      uint8_t c;
+    }
+    uint16_t bc;  
+  };
+  union {
+    struct {
+      uint8_t d;
+      uint8_t e;
+    }
+    uint16_t de;  
+  };
+  union {
+    struct {
+      uint8_t h;
+      uint8_t l;
+    }
+    uint16_t hl;  
+  };
+  uint8_t sp;
+  uint8_t pc;
+}
+
+#endif // !REGISTERS
