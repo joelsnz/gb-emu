8d6b3a16869bba32ae0eecb25ade9c82fd9c9b70
joelsnz <joel04slm@gmail.com>
2025-03-05 14:57:54 +0100

    Complete add function

--- src/cpu.c
+++ src/cpu.c
@@ -6,17 +6,19 @@
 
 // 8-bit arithmetic
 void add(const uint8_t value) {
-  uint16_t result = registers.a + value;
+  const uint16_t result = registers.a + value;
 
   if(result) CLEAR_FLAG(ZERO_FLAG);
   else SET_FLAG(ZERO_FLAG);
 
   CLEAR_FLAG(NEGATIVE_FLAG);
 
-  uint8_t half_carry = (registers.a & 0x0F) + (value & 0x0F) > 0x0F;
+  const uint8_t half_carry = (registers.a & 0x0F) + (value & 0x0F) > 0x0F;
   if(half_carry) SET_FLAG(CARRY_FLAG);
   else CLEAR_FLAG(CARRY_FLAG);
 
   if(result > UINT8_MAX) SET_FLAG(CARRY_FLAG);
   else CLEAR_FLAG(CARRY_FLAG);
+
+  registers.a = (uint8_t) result;
 }
