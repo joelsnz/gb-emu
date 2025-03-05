12a12edc7026c10dcabaeaf49062985497972ae5
joelsnz <joel04slm@gmail.com>
2025-03-05 19:21:32 +0100

    Add sub and sbc

--- src/cpu.c
+++ src/cpu.c
@@ -41,11 +41,39 @@ void adc(const uint8_t value) {
 }
 
 void sub(const uint8_t value) {
+  const uint16_t result = registers.a - value;
 
+  if(result) CLEAR_FLAG(ZERO_FLAG);
+  else SET_FLAG(ZERO_FLAG);
+
+  SET_FLAG(NEGATIVE_FLAG);
+
+  const uint8_t half_carry = (registers.a & 0x0F) - (value & 0x0F) > 0x0F;
+  if(half_carry) SET_FLAG(CARRY_FLAG);
+  else CLEAR_FLAG(CARRY_FLAG);
+
+  if(result > UINT8_MAX) SET_FLAG(CARRY_FLAG);
+  else CLEAR_FLAG(CARRY_FLAG);
+
+  registers.a = (uint8_t) result;
 }
 
 void sbc(const uint8_t value) {
+  const uint16_t result = registers.a - value - ISSET_FLAG(CARRY_FLAG);
 
+  if(result) CLEAR_FLAG(ZERO_FLAG);
+  else SET_FLAG(ZERO_FLAG);
+
+  CLEAR_FLAG(NEGATIVE_FLAG);
+
+  const uint8_t half_carry = (registers.a & 0x0F) - (value & 0x0F) > 0x0F;
+  if(half_carry) SET_FLAG(CARRY_FLAG);
+  else CLEAR_FLAG(CARRY_FLAG);
+
+  if(result > UINT8_MAX) SET_FLAG(CARRY_FLAG);
+  else CLEAR_FLAG(CARRY_FLAG);
+
+  registers.a = (uint8_t) result;
 }
 
 void and(const uint8_t value) {
