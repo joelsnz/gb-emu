89c4d64458525742ba644b2096323596079aea51
Joel Sanz Martí <joel04slm@gmail.com>
2025-04-14 13:06:49 +0200

    implement instruction execution

--- src/main.c
+++ src/main.c
@@ -1,4 +1,20 @@
+#include <cpu.h>
+#include <memory.h>
+#include <registers.h>
+#include <stdio.h>
+
 int main(void) {
+  memory[0] = 0xCB;
+  memory[1] = 0x00;
+  registers.pc = 0x0;
+  registers.a = 0x03;
+  registers.b = 0x04;
+
+  printf("a: %d\tb: %d\tpc: %d\n", registers.a, registers.b, registers.pc);
+
+  step();
+
+  printf("a: %d\tb: %d\tpc: %d\n", registers.a, registers.b, registers.pc);
 
   return 0;
 }
--- src/memory.c
+++ src/memory.c
@@ -1,3 +1,3 @@
 #include "memory.h"
 
-uint8_t memory[0x10000];
+uint8_t memory[0xFFFF + 1];
