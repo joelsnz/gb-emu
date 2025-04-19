def4704c02ca56e17206542983a1533dba90c82d
joelsnz <joel04slm@gmail.com>
2025-04-19 20:55:58 +0200

    unify shifts

--- include/shifts.h
+++ include/shifts.h
@@ -3,16 +3,16 @@
 
 #include <stdint.h>
 
-void rlca(void);
-void rla(void);
-void rrca(void);
-void rra(void);
-void rlc(uint8_t *reg);
-void rl(uint8_t *reg);
-void rrc(uint8_t *reg);
-void rr(uint8_t *reg);
-void sla(uint8_t *reg);
-void sra(uint8_t *reg);
-void srl(uint8_t *reg);
+void rlca(cpu_t *cpu);
+void rla(cpu_t *cpu);
+void rrca(cpu_t *cpu);
+void rra(cpu_t *cpu);
+void rlc(cpu_t *cpu);
+void rl(cpu_t *cpu);
+void rrc(cpu_t *cpu);
+void rr(cpu_t *cpu);
+void sla(cpu_t *cpu);
+void sra(cpu_t *cpu);
+void srl(cpu_t *cpu);
 
 #endif //SHIFTS_H
--- src/shifts.c
+++ src/shifts.c
@@ -1,171 +1,177 @@
-#include "registers.h"
-#include "memory.h"
-#include "flags.h"
-
 #include "shifts.h"
 
-void rlca(void) {
-	uint8_t result = registers.a << 1;
+#include "cpu.h"
+#include "flags.h"
 
-	CLEAR_FLAG(ZERO_FLAG | NEGATIVE_FLAG | HALFCARRY_FLAG);
+void rlca(cpu_t *cpu) {
+  uint8_t result = cpu->registers.a << 1;
 
-	if(registers.a & 0x80) {
-		SET_FLAG(CARRY_FLAG);
-		result++;
-	} else {
-		CLEAR_FLAG(CARRY_FLAG);
-	}
+  CLEAR_FLAG(cpu, ZERO_FLAG | NEGATIVE_FLAG | HALFCARRY_FLAG);
 
-	registers.a = result;
+  if(cpu->registers.a & 0x80) {
+    SET_FLAG(cpu, CARRY_FLAG);
+    result++;
+  } else {
+    CLEAR_FLAG(cpu, CARRY_FLAG);
+  }
+
+  cpu->registers.a = result;
 }
 
-void rla(void) {
-	uint8_t result = registers.a << 1;
+void rla(cpu_t *cpu) {
+  uint8_t result = cpu->registers.a << 1;
 
-	result += ISSET_FLAG(CARRY_FLAG);
+  result += ISSET_FLAG(cpu, CARRY_FLAG);
 
-	CLEAR_FLAG(ZERO_FLAG | NEGATIVE_FLAG | HALFCARRY_FLAG);
+  CLEAR_FLAG(cpu, ZERO_FLAG | NEGATIVE_FLAG | HALFCARRY_FLAG);
 
-	if(registers.a & 0x80) SET_FLAG(CARRY_FLAG);
-	else CLEAR_FLAG(CARRY_FLAG);
+  if(cpu->registers.a & 0x80) SET_FLAG(cpu, CARRY_FLAG);
+  else CLEAR_FLAG(cpu, CARRY_FLAG);
 
-	registers.a = result;
+  cpu->registers.a = result;
 }
 
-void rrca(void) {
-	uint8_t result = registers.a >> 1;
+void rrca(cpu_t *cpu) {
+  uint8_t result = cpu->registers.a >> 1;
 
-	CLEAR_FLAG(ZERO_FLAG | NEGATIVE_FLAG | HALFCARRY_FLAG);
+  CLEAR_FLAG(cpu, ZERO_FLAG | NEGATIVE_FLAG | HALFCARRY_FLAG);
 
-	if(registers.a & 0x01) {
-		SET_FLAG(CARRY_FLAG);
-		result |= 0x80;
-	} else {
-		CLEAR_FLAG(CARRY_FLAG);
-	}
+  if(cpu->registers.a & 0x01) {
+    SET_FLAG(cpu, CARRY_FLAG);
+    result |= 0x80;
+  } else {
+    CLEAR_FLAG(cpu, CARRY_FLAG);
+  }
 
-	registers.a = result;
+  cpu->registers.a = result;
 }
 
-void rra(void) {
-	uint8_t result = registers.a >> 1;
+void rra(cpu_t *cpu) {
+  uint8_t result = cpu->registers.a >> 1;
 
-	result += ISSET_FLAG(CARRY_FLAG) ? 0x80 : 0x00;
+  result += ISSET_FLAG(cpu, CARRY_FLAG) ? 0x80 : 0x00;
 
-	CLEAR_FLAG(ZERO_FLAG | NEGATIVE_FLAG | HALFCARRY_FLAG);
+  CLEAR_FLAG(cpu, ZERO_FLAG | NEGATIVE_FLAG | HALFCARRY_FLAG);
 
-	if(registers.a & 0x01) SET_FLAG(CARRY_FLAG);
-	else CLEAR_FLAG(CARRY_FLAG);
+  if(cpu->registers.a & 0x01) SET_FLAG(cpu, CARRY_FLAG);
+  else CLEAR_FLAG(cpu, CARRY_FLAG);
 
-	registers.a = result;
+  cpu->registers.a = result;
 }
 
-void rlc(uint8_t *reg) {
-	uint8_t result = *reg << 1;
-
-	CLEAR_FLAG(NEGATIVE_FLAG | HALFCARRY_FLAG);
-
-	if(*reg & 0x80) {
-		SET_FLAG(CARRY_FLAG);
-		result++;
-	} else {
-		CLEAR_FLAG(CARRY_FLAG);
-	}
-
-  if(result) CLEAR_FLAG(ZERO_FLAG);
-  else SET_FLAG(ZERO_FLAG);
-
-	*reg = result;
-}
-
-void rl(uint8_t *reg) {
-	uint8_t result = *reg << 1;
-
-	result += ISSET_FLAG(CARRY_FLAG);
-
-	CLEAR_FLAG(NEGATIVE_FLAG | HALFCARRY_FLAG);
-
-	if(*reg & 0x80) SET_FLAG(CARRY_FLAG);
-	else CLEAR_FLAG(CARRY_FLAG);
-
-  if(result) CLEAR_FLAG(ZERO_FLAG);
-  else SET_FLAG(ZERO_FLAG);
-
-	*reg = result;
-}
-
-void rrc(uint8_t *reg) {
-	uint8_t result = *reg >> 1;
-
-	CLEAR_FLAG(NEGATIVE_FLAG | HALFCARRY_FLAG);
-
-	if(*reg & 0x01) {
-		SET_FLAG(CARRY_FLAG);
-		result |= 0x80;
-	} else {
-		CLEAR_FLAG(CARRY_FLAG);
-	}
-
-  if(result) CLEAR_FLAG(ZERO_FLAG);
-  else SET_FLAG(ZERO_FLAG);
-
-	*reg = result;
-}
-
-void rr(uint8_t *reg) {
-	uint8_t result = *reg >> 1;
-
-	result += ISSET_FLAG(CARRY_FLAG) ? 0x80 : 0x00;
-
-	CLEAR_FLAG(NEGATIVE_FLAG | HALFCARRY_FLAG);
-
-	if(*reg & 0x01) SET_FLAG(CARRY_FLAG);
-	else CLEAR_FLAG(CARRY_FLAG);
-
-  if(result) CLEAR_FLAG(ZERO_FLAG);
-  else SET_FLAG(ZERO_FLAG);
-
-	*reg = result;
-}
-
-void sla(uint8_t *reg) {
+void rlc(cpu_t *cpu) {
+  uint8_t *reg = *get_lower_r8(cpu);
   uint8_t result = *reg << 1;
 
-  if(result) CLEAR_FLAG(ZERO_FLAG);
-  else SET_FLAG(ZERO_FLAG);
+  CLEAR_FLAG(cpu, NEGATIVE_FLAG | HALFCARRY_FLAG);
 
-	CLEAR_FLAG(NEGATIVE_FLAG | HALFCARRY_FLAG);
+  if(*reg & 0x80) {
+    SET_FLAG(cpu, CARRY_FLAG);
+    result++;
+  } else {
+    CLEAR_FLAG(cpu, CARRY_FLAG);
+  }
 
-  if(*reg & 0x80) SET_FLAG(CARRY_FLAG);
-  else CLEAR_FLAG(CARRY_FLAG);
-
-  *reg = result;
-}
-
-void sra(uint8_t *reg) {
-  uint8_t result = (*reg >> 1) | (*reg & 0x80);
-
-  if(result) CLEAR_FLAG(ZERO_FLAG);
-  else SET_FLAG(ZERO_FLAG);
-
-	CLEAR_FLAG(NEGATIVE_FLAG | HALFCARRY_FLAG);
-
-  if(*reg & 0x01) SET_FLAG(CARRY_FLAG);
-  else CLEAR_FLAG(CARRY_FLAG);
+  if(result) CLEAR_FLAG(cpu, ZERO_FLAG);
+  else SET_FLAG(cpu, ZERO_FLAG);
 
   *reg = result;
 }
 
-void srl(uint8_t *reg) {
+void rl(cpu_t *cpu) {
+  uint8_t *reg = *get_lower_r8(cpu);
+  uint8_t result = *reg << 1;
+
+  result += ISSET_FLAG(cpu, CARRY_FLAG);
+
+  CLEAR_FLAG(cpu, NEGATIVE_FLAG | HALFCARRY_FLAG);
+
+  if(*reg & 0x80) SET_FLAG(cpu, CARRY_FLAG);
+  else CLEAR_FLAG(cpu, CARRY_FLAG);
+
+  if(result) CLEAR_FLAG(cpu, ZERO_FLAG);
+  else SET_FLAG(cpu, ZERO_FLAG);
+
+  *reg = result;
+}
+
+void rrc(cpu_t *cpu) {
+  uint8_t *reg = *get_lower_r8(cpu);
   uint8_t result = *reg >> 1;
 
-  if(result) CLEAR_FLAG(ZERO_FLAG);
-  else SET_FLAG(ZERO_FLAG);
+  CLEAR_FLAG(cpu, NEGATIVE_FLAG | HALFCARRY_FLAG);
 
-	CLEAR_FLAG(NEGATIVE_FLAG | HALFCARRY_FLAG);
+  if(*reg & 0x01) {
+    SET_FLAG(cpu, CARRY_FLAG);
+    result |= 0x80;
+  } else {
+    CLEAR_FLAG(cpu, CARRY_FLAG);
+  }
 
-  if(*reg & 0x01) SET_FLAG(CARRY_FLAG);
-  else CLEAR_FLAG(CARRY_FLAG);
+  if(result) CLEAR_FLAG(cpu, ZERO_FLAG);
+  else SET_FLAG(cpu, ZERO_FLAG);
+
+  *reg = result;
+}
+
+void rr(cpu_t *cpu) {
+  uint8_t *reg = *get_lower_r8(cpu);
+  uint8_t result = *reg >> 1;
+
+  result += ISSET_FLAG(cpu, CARRY_FLAG) ? 0x80 : 0x00;
+
+  CLEAR_FLAG(cpu, NEGATIVE_FLAG | HALFCARRY_FLAG);
+
+  if(*reg & 0x01) SET_FLAG(cpu, CARRY_FLAG);
+  else CLEAR_FLAG(cpu, CARRY_FLAG);
+
+  if(result) CLEAR_FLAG(cpu, ZERO_FLAG);
+  else SET_FLAG(cpu, ZERO_FLAG);
+
+  *reg = result;
+}
+
+void sla(cpu_t *cpu) {
+  uint8_t *reg = *get_lower_r8(cpu);
+  uint8_t result = *reg << 1;
+
+  if(result) CLEAR_FLAG(cpu, ZERO_FLAG);
+  else SET_FLAG(cpu, ZERO_FLAG);
+
+  CLEAR_FLAG(cpu, NEGATIVE_FLAG | HALFCARRY_FLAG);
+
+  if(*reg & 0x80) SET_FLAG(cpu, CARRY_FLAG);
+  else CLEAR_FLAG(cpu, CARRY_FLAG);
+
+  *reg = result;
+}
+
+void sra(cpu_t *cpu) {
+  uint8_t *reg = *get_lower_r8(cpu);
+  uint8_t result = (*reg >> 1) | (*reg & 0x80);
+
+  if(result) CLEAR_FLAG(cpu, ZERO_FLAG);
+  else SET_FLAG(cpu, ZERO_FLAG);
+
+  CLEAR_FLAG(cpu, NEGATIVE_FLAG | HALFCARRY_FLAG);
+
+  if(*reg & 0x01) SET_FLAG(cpu, CARRY_FLAG);
+  else CLEAR_FLAG(cpu, CARRY_FLAG);
+
+  *reg = result;
+}
+
+void srl(cpu_t *cpu) {
+  uint8_t *reg = *get_lower_r8(cpu);
+  uint8_t result = *reg >> 1;
+
+  if(result) CLEAR_FLAG(cpu, ZERO_FLAG);
+  else SET_FLAG(cpu, ZERO_FLAG);
+
+  CLEAR_FLAG(cpu, NEGATIVE_FLAG | HALFCARRY_FLAG);
+
+  if(*reg & 0x01) SET_FLAG(cpu, CARRY_FLAG);
+  else CLEAR_FLAG(cpu, CARRY_FLAG);
 
   *reg = result;
 }
