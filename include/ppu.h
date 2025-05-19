7049f839b4c024ca70a54cc3e2658bb1269b55d1
Joel Sanz Martí <joel04slm@gmail.com>
2025-05-19 16:30:21 +0200

    debug

---
+++ include/ppu.h
@@ -0,0 +1,8 @@
+#ifndef PPU_H
+#define PPU_H
+
+#include <stdint.h>
+
+
+
+#endif //PPU_H
--- src/main.c
+++ src/main.c
@@ -7,16 +7,11 @@
 int main(int argc, char **argv) {
   cpu_t *cpu = malloc(sizeof(cpu_t));
   init_cpu(cpu);
-<<<<<<< HEAD
   init_video();
-  const char rom[] = "../../../Documents/gb_test_roms/01-special.gb";
-=======
-  const char rom[] = "../../../Documents/01-special.gb";
->>>>>>> 4475260e9a46d17d0d523c001fb6abac74450b27
-  load_rom(cpu, rom /*argv[1]*/);
+  // load_rom(cpu, rom argv[1]);
   boot_sequence(cpu);
 
-  while(video_step()) {};
+  while(video_step()) {}
 
   return 0;
 }
