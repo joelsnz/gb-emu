496e1875e2efc65286b6a62252c97072e4bce24d
joelsnz <joel04slm@gmail.com>
2025-06-25 22:35:15 +0200

    draw background and window layers

--- src/lcd.c
+++ src/lcd.c
@@ -2,12 +2,12 @@
 
 #include "SDL3/SDL.h"
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 static SDL_Window *window = NULL;
 static SDL_Surface *surface = NULL;
-static SDL_Rect pixel = {0, 0, WINDOW_SCALE, WINDOW_SCALE};
 
 void lcd_init() {
   SDL_Init(SDL_INIT_VIDEO);
@@ -17,20 +17,49 @@ void lcd_init() {
   surface = SDL_GetWindowSurface(window);
 }
 
-void lcd_draw_frame() {
+static SDL_Rect scale(SDL_Rect rect) {
+  rect.x *= WINDOW_SCALE;
+  rect.y *= WINDOW_SCALE;
+  rect.w *= WINDOW_SCALE;
+  rect.h *= WINDOW_SCALE;
+
+  return rect;
+}
+
+void lcd_draw_background() {
+  SDL_Rect pixel = scale((SDL_Rect){0, 0, 1, 1});
+
   for(pixel.x = 0; pixel.x < LCD_WIDTH * WINDOW_SCALE;
       pixel.x += WINDOW_SCALE)
     for(pixel.y = 0; pixel.y < LCD_HEIGHT * WINDOW_SCALE;
         pixel.y += WINDOW_SCALE)
-      SDL_FillSurfaceRect(surface, &pixel, (Uint32)rand());
+      SDL_FillSurfaceRect(surface, &pixel, (uint32_t)rand());
+}
 
-  SDL_UpdateWindowSurface(window);
+void lcd_draw_window(int x, int y, uint32_t color) {
+  SDL_Rect wdw =
+      scale((SDL_Rect){x, y, LCD_WIDTH - x, LCD_HEIGHT - y});
+  SDL_FillSurfaceRect(surface, &wdw, color);
 }
 
 void lcd_test() {
   lcd_init();
 
-  lcd_draw_frame();
-
-  SDL_Delay(5000);
+  int game = 1;
+  while(game) {
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+      switch(event.type) {
+      case SDL_EVENT_QUIT:
+        game = 0;
+        break;
+      default:
+        continue;
+      }
+    }
+    lcd_draw_background();
+    lcd_draw_window(30, 110, 0x55555555);
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(20);
+  }
 }
